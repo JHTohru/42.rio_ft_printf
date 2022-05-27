72609795f7c381b0432f391ca3834ce7148480cd
tohru <jhtohru@gmail.com>
2022-05-27 08:57:14 -0300

    convert_int normalizado

--- src/convert_int.c
+++ src/convert_int.c
@@ -10,72 +10,47 @@ int	ft_abs(int n)
 	return (n);
 }
 
-unsigned int	digitslen(unsigned int n)
+int	write_chars(char *str, int c, int n)
 {
-	unsigned int	len;
-
-	len = 0;
-	while (1)
-	{
-		len++;
-		n /= 10;
-		if (n == 0)
-			return (len);
-	}
-}
-
-int	write_padding(char *str, unsigned int n)
-{
-	ft_memset((void *)str, ' ', n);
+	ft_memset((void *)str, c, (size_t)n);
 	return (n);
 }
 
-int	write_zeros(char *str, unsigned int n)
+uint	upow(int b, int e)
 {
-	ft_memset((void *)str, '0', n);
-	return (n);
-}
+	uint	p;
 
-int	ft_log(int n, int b)
-{
-	int	log;
-
-	log = 0;
-	while (n > b)
-	{
-		n /= b;
-		log++;
-	}
-	return(log);
-}
-
-int	ft_pow(int b, unsigned int e)
-{
-	int	n;
-
-	n = 1;
+	p = 1;
 	while (e != 0)
 	{
-		n *= b;
+		p *= (uint)b;
 		e--;
 	}
-	return (n);
+	return (p);
 }
 
-unsigned int	ft_mag(int n, int b)
+uint	ulog(uint n, int b)
 {
-	return (ft_pow(b, ft_log(n, b)));
+	uint	lg;
+
+	lg = 0;
+	while (n > (uint)b)
+	{
+		n /= (uint)b;
+		lg++;
+	}
+	return(lg);
 }
 
-int	write_nbr(char *str, int n, char *baseset)
+int	uwrite(char *str, uint n, char *baseset)
 {
-	unsigned int	rad;
-	unsigned int	mag;
-	unsigned int	i;
+	uint	mag;
+	uint	rad;
+	int		i;
 
 	i = 0;
-	rad = (unsigned int)ft_strlen(baseset);
-	mag = ft_mag(n, rad);
+	rad = (uint)ft_strlen(baseset);
+	mag = upow(rad, ulog(n, rad));
 	while (1)
 	{
 		str[i++] = baseset[n / mag];
@@ -87,56 +62,79 @@ int	write_nbr(char *str, int n, char *baseset)
 	return (i);
 }
 
-int	convert_int(char **str, t_convspec *cs, int d)
+int	udigits(uint n, int r)
 {
-	int				padding_left;
-	int				padding_right;
-	int				leading_zeros;
-	int				flag_zero;
-	unsigned int	n;
-	int				nlen;
-	int				len;
+	int	cnt;
 
-	n = ft_abs(d);
-	if (n != 0 || !cs->flag_period || cs->precision > 0)
-		nlen = digitslen(n);
-	else
-		nlen = 0;
-	leading_zeros = 0;
-	if (cs->flag_period && cs->precision > nlen)
-		leading_zeros = cs->precision - nlen;
-	padding_left = 0;
-	if (d < 0 || cs->flag_plus)
-		nlen++;
-	else if (cs->flag_space)
-		padding_left++;
-	flag_zero = cs->flag_zero && !cs->flag_period && !cs->flag_minus
-		&& cs->field_width > nlen;
-	if (flag_zero)
-		leading_zeros = cs->field_width - nlen - padding_left;
-	nlen += leading_zeros;
-	padding_right = 0;
-	if (cs->field_width > nlen)
+	cnt = 0;
+	while (1)
 	{
-		if (cs->flag_minus)
-			padding_right = cs->field_width - nlen;
-		else if (!flag_zero)
-			padding_left += cs->field_width - nlen;
+		cnt++;
+		n /= (uint)r;
+		if (n == 0)
+			return (cnt);
 	}
-	len = padding_left + nlen + padding_right;
-	*str = malloc((len + 1) * sizeof(char));
-	if (*str != NULL)
-	{	
-		len = write_padding(*str, padding_left);
-		if (d < 0)
-			(*str)[len++] = '-';
-		else if (cs->flag_plus)
-			(*str)[len++] = '+';
-		len += write_zeros(*str + len, leading_zeros);
-		if (nlen > 0)
-			len += write_nbr(*str + len, n, BASESET_DEC);
-		len += write_padding(*str + len, padding_right);
-		(*str)[len] = '\0';
-	}
+}
+
+int	nbrlen(t_convspec *cs, int n)
+{
+	int	len;
+
+	if (n == 0 && cs->flag_period && cs->precision == 0)
+		len = 0;
+	else
+		len = udigits((uint)ft_abs(n), 10);
+	if (cs->flag_period && cs->precision > len)
+		len = cs->precision;
+	if (n < 0 || cs ->flag_plus || cs->flag_space)
+		len++;
 	return (len);
+}
+
+int	write_nbr(char *str, t_convspec *cs, int n)
+{
+	int	i;
+	int	len;
+
+	len = udigits((uint)ft_abs(n), 10);
+	i = 0;
+	if (n >= 0 && cs->flag_plus)
+		str[i++] = '+';
+	else if (n >= 0 && cs->flag_space)
+		str[i++] = ' ';
+	else if (n < 0)
+		str[i++] = '-';
+	if (cs->flag_zero && !cs->flag_period && !cs->flag_minus
+		&& cs->field_width > len + i)
+		i += write_chars(str + i, '0', cs->field_width - len - i);
+	else if (cs->flag_period && cs->precision > len)
+		i += write_chars(str + i, '0', cs->precision - len);
+	if (!cs->flag_period || cs->precision > 0 || n != 0)
+		i += uwrite(str + i, (uint)ft_abs(n), BASESET_DEC);
+	return (i);
+}
+
+int	convert_int(char **str, t_convspec *cs, int n)
+{
+	int	nlen;
+	int i;
+
+	nlen = nbrlen(cs, n);
+	if (cs->field_width > nlen)
+		*str = malloc(cs->field_width + 1);
+	else
+		*str = malloc(nlen + 1);
+	i = 0;
+	if (*str != NULL)
+	{
+		if (cs->field_width > nlen && (!cs->flag_zero || cs->flag_period)
+			&& !cs->flag_minus)
+			i = write_chars(*str, ' ', cs->field_width - nlen);
+		i += write_nbr(*str + i, cs, n);
+		if (cs->field_width > nlen && cs->flag_minus)
+			i += write_chars(*str + i, ' ', cs->field_width - nlen);
+		(*str)[i] = '\0';
+	}
+	// todo: o que retornar quando *str == NULL?
+	return (i);
 }
